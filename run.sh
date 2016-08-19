11ef9fa06f8b25f469188983f2c6de0fad15501b
Ivo Lukac <ivo@netgen.hr>
2016-08-19 16:35:40 +0200

    adding first 6 workshops as submodules

--- run.sh
+++ run.sh
@@ -1,5 +1,71 @@
 #!/usr/bin/env bash
 
+# oop setup
+function phpstorm_setup() {
+  echo "Running phpstorm workshop setup..."
+  ( cd workshops/phpstorm &&
+    git checkout master &&
+    git pull origin master &&
+    sudo ln -sf /var/www/summercamp/workshops/phpstorm/installation/vhost /etc/apache2/sites-enabled/phpstorm.conf &&
+    sudo sh ./installation/run.sh
+  )
+}
+
+# ubiquitous setup
+function ubiquitous_setup() {
+  echo "Running ubiquitous workshop setup..."
+  ( cd workshops/ubiquitous &&
+    git checkout master &&
+    git pull origin master &&
+    sudo ln -sf /var/www/summercamp/workshops/ubiquitous/installation/vhost /etc/apache2/sites-enabled/ubiquitous.conf &&
+    sudo sh ./installation/run.sh
+  )
+}
+
+# httplug setup
+function httplug_setup() {
+  echo "Running httplug workshop setup..."
+  ( cd workshops/httplug &&
+    git checkout master &&
+    git pull origin master &&
+    sudo ln -sf /var/www/summercamp/workshops/httplug/installation/vhost /etc/apache2/sites-enabled/httplug.conf &&
+    sudo sh ./installation/run.sh
+  )
+}
+
+# sulu setup
+function sulu_setup() {
+  echo "Running sulu workshop setup..."
+  ( cd workshops/sulu &&
+    git checkout master &&
+    git pull origin master &&
+    sudo ln -sf /var/www/summercamp/workshops/sulu/installation/vhost /etc/apache2/sites-enabled/sulu.conf &&
+    sudo sh ./installation/run.sh
+  )
+}
+
+# api4ez setup
+function api4ez_setup() {
+  echo "Running api4ez workshop setup..."
+  ( cd workshops/api4ez &&
+    git checkout master &&
+    git pull origin master &&
+    sudo ln -sf /var/www/summercamp/workshops/api4ez/installation/vhost /etc/apache2/sites-enabled/api4ez.conf &&
+    sudo sh ./installation/run.sh
+  )
+}
+
+# headless setup
+function headless_setup() {
+  echo "Running headless workshop setup..."
+  ( cd workshops/headless &&
+    git checkout master &&
+    git pull origin master &&
+    sudo ln -sf /var/www/summercamp/workshops/headless/installation/vhost /etc/apache2/sites-enabled/headless.conf &&
+    sudo sh ./installation/run.sh
+  )
+}
+
 # ezsylius setup
 #function ezsylius_setup() {
 #  echo "Running ezsylius workshop setup..."
@@ -13,7 +79,7 @@
 
 
 function all() {
-#  ezsylius_setup
+  phpstorm_setup
 }
 
 echo "Running github token setup..."
