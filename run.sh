018b3b7139a61763342aafad0197a676e6a560f4
Edi Modrić <edi@netgen.hr>
2016-09-09 10:54:48 +0200

    Revert "Disable the script for now, no need to run it on websc"
    This reverts commit ff85d89dc0cf9932bda9f33af76aed90f894b6d7.

--- run.sh
+++ run.sh
@@ -201,25 +201,25 @@ function all() {
   phpstorm_setup
 }
 
-# echo "Running github token setup..."
+echo "Running github token setup..."
 # PLEASE DO NOT USE THIS TOKEN IN YOUR OWN PROJECTS/FORKS",
 # This token is reserved for fetching summer camp repositories",
 # You create your own token without(!) any scope to use same approach",
-# composer config -g github-oauth.github.com "4dd9fefd938753073b2915b2ecd052d288370e8f"
+composer config -g github-oauth.github.com "4dd9fefd938753073b2915b2ecd052d288370e8f"
 
 # setup the submodules
-# git submodule init
-# git submodule update
+git submodule init
+git submodule update
 
 # get argument
-# if [ -z "$1" ]
-# then
-#    all
-# else
-#   "$1_setup"
-# fi
+if [ -z "$1" ]
+then
+   all
+else
+  "$1_setup"
+fi
 
-# source ~/.bashrc
+source ~/.bashrc
 
-# echo "Reloading apache..."
-# sudo service apache2 restart
+echo "Reloading apache..."
+sudo service apache2 restart
